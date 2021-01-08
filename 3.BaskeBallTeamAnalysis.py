9b916faf1b2b7a1d9d42cc74d4e7d8a0712d9eb3
98sunny <sunnys.sir@gmail.com>
2021-01-08 18:36:04 -0500

    Added more features
    Side bar added with other filters like team and position filters.
    Created the downloading csv functionality
    Completed the heat ap function.

--- 3.BaskeBallTeamAnalysis.py
+++ 3.BaskeBallTeamAnalysis.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import matplotlib.pyplot as plt
-imprt seaborn as sns
+import seaborn as sns
 import numpy as np
 import base64
 import pandas as pd
-import seaborn as sns
 
 st.title('NBA Player Stats Explorer')
 
@@ -14,7 +13,61 @@ Python Libraries used: base6,streamlit, pandas.
 Date Scores: https://www.basketball-reference.com/
 
 """
-)
+            )
 
-st.sidebar.heade('User Input Feaures')
-selected_year=st.sidebar('Year',list(reversed(range(1950,2021))))
+st.sidebar.header('User Input Feaures')
+selected_year = st.sidebar.selectbox('Year', list(reversed(range(1950, 2020))))
+
+
+@st.cache
+def load_data(year):
+    url = "https://www.basketball-reference.com/leagues/NBA_" + \
+        str(year) + "_per_game.html"
+    html = pd.read_html(url, header=0)
+    df = html[0]
+    raw = df.drop(df[df.Age == 'Age'].index)
+    raw = raw.fillna(0)
+    playerstats = raw.drop(['Rk'], axis=1)
+    return playerstats
+
+
+playerstats = load_data(selected_year)
+print(playerstats)
+sorted_unique_team = sorted(playerstats.Tm.unique())
+seleccted_team = st.sidebar.multiselect(
+    'Team', sorted_unique_team, sorted_unique_team)
+# Sidebar - Position selection
+unique_pos = ['C', 'PF', 'SF', 'PG', 'SG']
+selected_pos = st.sidebar.multiselect('Position', unique_pos, unique_pos)
+
+df_selected_team = playerstats[(playerstats.Tm.isin(
+    seleccted_team)) & (playerstats.Pos.isin(selected_pos))]
+st.header('Display Player Stats of Selected Team(s)')
+st.write('Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(
+    df_selected_team.shape[1]) + ' columns.')
+st.dataframe(df_selected_team)
+
+
+def filedownload(df):
+    csv = df.to_csv(index=False)
+    # strings <-> bytes conversions
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="playerstats.csv">Download CSV File</a>'
+    return href
+
+
+st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
+
+# Heatmap
+if st.button('Intercorrelation Heatmap'):
+    st.header('Intercorrelation Matrix Heatmap')
+    df_selected_team.to_csv('output.csv', index=False)
+    df = pd.read_csv('output.csv')
+
+    corr = df.corr()
+    mask = np.zeros_like(corr)
+    mask[np.triu_indices_from(mask)] = True
+    with sns.axes_style("white"):
+        f, ax = plt.subplots(figsize=(7, 5))
+        ax = sns.heatmap(corr, mask=mask, vmax=1, square=True)
+    st.pyplot()
